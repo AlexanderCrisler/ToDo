1078f07d5aacdebae81996c8cf21dce24c58fe0f
AlexanderCrisler <alexcrisler@gmail.com>
2023-02-26 15:56:10 -0800

    Add some boiler plate code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,32 @@ const createPage = () => {
 	page.textContent= 'Hello World!';
 
 	document.body.appendChild(page);
+	document.body.appendChild(projectsList());
 };
+
+const projectsList = () => {
+	let projects = document.createElement('div');
+	projects.id = 'projectsList';
+
+	// Add list of projects
+	
+	// Add new project button
+	let addButton = document.createElement('div');
+	addButton.textContent = '+';
+	addButton.addEventListener(onclick, createProject());
+
+	projects.appendChild(addButton);
+
+	return projects;
+};
+
+// TODO: create Project class
+// This should be a class method
+const createProject = () => {
+	let project = document.createElement('div');
+	project.class = 'project';
+	
+	return project;
+}
 
 createPage();
